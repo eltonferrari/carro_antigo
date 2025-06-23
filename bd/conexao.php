0b5325e728cccfb3bfe2238a4cfcf3f410ee5818
Elton Ferrari <eltonferrari@gmail.com>
2025-06-23 15:48:35 -0300

    Commit Inicial

---
+++ bd/conexao.php
@@ -0,0 +1,10 @@
+<?php
+// Created by Elton Ferrari
+ 
+$databaseHost = "127.0.0.1";
+$databaseName = "carros_antigos";
+$databaseUsername = "root";
+$databasePassword = "";
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
